fc5d71c27608c3e9f5f23a9409bb4d95cbc7d93e
Rafael Paiva <rafa_klose@hotmail.com>
2021-02-02 01:20:29 -0300

    added header

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import Header from '../../components/Header';
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Login;
